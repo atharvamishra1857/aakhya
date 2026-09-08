65423ccb626fe11d7b01a4b8c4698317a2c1f236
Atharva Mishra <mishraatharva1857@gmail.com>
2026-09-08 11:08:27 +0530

    still on it

--- app/api/payu/hash/route.ts
+++ app/api/payu/hash/route.ts
@@ -11,7 +11,8 @@ export async function POST(req: NextRequest) {
     const email = String(body.email || "").trim();
     const udf1 = String(body.udf1 || "").trim();
 
-    const salt = (process.env.PAYU_SALT || "").trim();
+    // const salt = (process.env.PAYU_SALT || "").trim();
+    const salt = "dCWmlOTvp6upSuBPDwsjkQmwRWGEZ4i8";
     const key = (process.env.NEXT_PUBLIC_PAYU_KEY || "").trim();
 
     if (!key || !salt) {
